0476baa137966128fecb0a7c68dc5967eb4bdde9
yechang <zhangyechang@ad2o.com>
2017-08-18 17:52:34 +0800

    添加ngx-boostrap datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { BuyOrderComponent } from './buy-order/buy-order.component';
 import { EstimateComponent } from './estimate/estimate.component';
+
 
 // 路由
 import { AppRoutingModule } from './app-routing.module';
 import { HotelComponent } from './hotel/hotel.component';
 
+//ngx-boostrap
+import { DatepickerModule } from 'ngx-bootstrap';
+import { DatepickerComponent } from './datepicker/datepicker.component';
 @NgModule({
   declarations: [
     AppComponent,
     BuyOrderComponent,
     EstimateComponent,
-    HotelComponent
+    HotelComponent,
+    DatepickerComponent
 
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DatepickerModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hotel/hotel.component.ts
+++ src/app/hotel/hotel.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DatepickerComponent } from './../datepicker/datepicker.component';
+import { fadeIn } from './../animate/fadeIn';
+import { handleMask } from './../animate/handleMask';
 @Component({
   selector: 'app-hotel',
   templateUrl: './hotel.component.html',
-  styleUrls: ['./hotel.component.css']
+  styleUrls: ['./hotel.component.css'],
+  animations: [fadeIn, handleMask]
 })
 export class HotelComponent implements OnInit {
+  showOverlay = false;
+
+  showOverlayPanel() { 
+    this.showOverlay = true;
+  }
+  closeAllPanel() {
+    this.handleDate = false;
+    this.showOverlay = false;
+  }
+  // 日期控件
+  public handleDate: boolean = false;
+  showDate(){
+    this.showOverlayPanel()
+    this.handleDate = true;
+  }
 
   constructor() { }
 
